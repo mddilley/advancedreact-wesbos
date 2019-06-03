053ad9d4b5faece975030fc806f13ad9aaf37afe
Michael Dilley <mike.dilley@gmail.com>
2019-06-03 16:55:45 -0500

    Complete Module 3, add Item to datamodel.graphql and deploy to Prisma, add item query and mutation to schema.graphql, add createItem and items resolvers

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,14 @@
 const Mutations = {
-    createDog(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
-        // create a dog
-        const newDog = { name: args.name };
-        global.dogs.push(newDog);
-        return newDog;
-        console.log(args);
+    async createItem(parent, args, ctx, info) {
+        //TODO checked if user is logged in
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
     }
 };
 
