388c532179922a874232738dda40b1ba8bc336a0
Michael Dilley <mike.dilley@gmail.com>
2019-06-07 16:12:07 -0500

    Finish video 25, update datamodel.graphql in backend and deploy to Prisma server, update schema.graphql with signup mutation, write signup() in Mutation.js to create user, salt and hash pw with bcrypt and create cookie and user token for login

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
     async createItem(parent, args, ctx, info) {
         //TODO checked if user is logged in
@@ -34,7 +37,30 @@ const Mutations = {
         // TODO
         // 3. Delete it! 
         return ctx.db.mutation.deleteItem({ where }, info);
-    }
+    },
+    async signup(parent, args, ctx, info) {
+        // lowercase the email
+        args.email = args.email.toLowerCase();
+        // hash their password
+        const password = await bcrypt.hash(args.password, 10);
+        // create the user in the DB
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                permissions: { set: ['USER'] },
+                }
+            }, info
+        );
+        // create the JWT token for them
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // set jwt as a cookie on the response
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+        });       
+        return user;     
+    },
 };
 
 module.exports = Mutations;
